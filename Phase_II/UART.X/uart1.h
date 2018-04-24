c99bbaf46de5dc1f0d8369f817886937d72f13c5
Pedro Martins <martinspedro@ua.pt>
2018-04-24 19:18:01 +0100

    Add sendchar header to uart1.h

--- Phase_II/UART.X/uart1.h
+++ Phase_II/UART.X/uart1.h
@@ -15,6 +15,7 @@ extern "C" {
 void config_UART1(uint32_t baudrate, uint8_t data_bits, unsigned char parity, uint8_t stop_bits);
 void enable_UART1(void);
 uint8_t can_read_UART1(void);
+uint8_t send_char(unsigned char c);
 
 #ifdef	__cplusplus
 }
