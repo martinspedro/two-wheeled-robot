6ab3c039d8844699442b24e9668f6153d15ce983
Pedro Martins <martinspedro@ua.pt>
2018-05-05 17:05:51 +0100

    Update main to perform some debug

--- Phase_II/Encoder.X/main.c
+++ Phase_II/Encoder.X/main.c
@@ -8,26 +8,50 @@
 
 #include "../devcfgx_config_bits.h"
 #include "../global.h"
-#include "interrupts.h"
-#include "encoder.h"
+#include "../interrupts.h"
 #include "../UART.X/uart1.h"
+#include "encoder.h"
 
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void main(void) {
     TRISAbits.TRISA3 = 0;
-    PORTAbits.RA3 = 0;
-    TRISDbits.TRISD0 = 1;
+    PORTAbits.RA3 = 1;
+    TRISBbits.TRISB4 = 0;
     
     config_UART1(115200, 8, 'N', 1);
     enable_UART1();
     send_char('S');
+    
     configure_global_interrupts();
     configure_external_interrupts();
     
     Enable_Global_Interrupts();
-    //ENABLE_EXT_INT1;
+    ENABLE_EXT_INT1;
     ENABLE_EXT_INT2;
     
-    while(1);
+    while(1)
+    {
+        
+        if (print_flag == 1)
+        {
+            print_uint8(CS_left);
+            send_char('\n');
+            /*
+            if(invalid_encoder_transition == ENCODER_ERROR)
+            {
+                send_char('E');
+                invalid_encoder_transition = ENCODER_SUCCESS;
+            }
+            
+            print_uint16(pulse_count_L);
+            send_char('\n');
+            */
+            print_flag = 0;
+        }
+        
+    }
 }
 
