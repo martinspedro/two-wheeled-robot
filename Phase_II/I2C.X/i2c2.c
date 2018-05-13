3da866270aea2705117fd550b29d80afcc730229
André Gradim <andregradim@gmail.com>
2018-05-13 20:45:04 +0100

    [I2C] Added functions to handle bit writing

--- Phase_II/I2C.X/i2c2.c
+++ Phase_II/I2C.X/i2c2.c
@@ -392,6 +392,36 @@ uint8_t writeBytes(uint8_t devAddr, uint8_t reg, uint8_t n_bytes, uint8_t* buffe
     return 0;
 }
 
+uint8_t writeBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t data) {
+    //      010 value to write
+    // 76543210 bit numbers
+    //    xxx   args: bitStart=4, length=3
+    // 00011100 mask byte
+    // 10101111 original value (sample)
+    // 10100011 original & ~mask
+    // 10101011 masked | value
+    uint8_t b;
+    if (readBytes(devAddr, regAddr,1, &b) == 0) {
+        uint8_t mask = ((1 << length) - 1) << (bitStart - length + 1);
+        data <<= (bitStart - length + 1); // shift data into correct position
+        data &= mask; // zero all non-important bits in data
+        b &= ~(mask); // zero all important bits in existing byte
+        b |= data; // combine data with existing byte
+        writeBytes(devAddr, regAddr,1, &b);
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+writeBit(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint8_t data) {
+    uint8_t b;
+    readBytes(devAddr, regAddr,1, &b);
+    b = (data != 0) ? (b | (1 << bitNum)) : (b & ~(1 << bitNum));
+    return writeBytes(devAddr, regAddr,1, &b);
+}
+
+
 
 
 
