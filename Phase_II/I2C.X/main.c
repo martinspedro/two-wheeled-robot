dc3c1158292816a6eb65702a0aa6ad1c84d5271b
André Gradim <andregradim@gmail.com>
2018-05-15 09:23:22 +0100

    [I2C] Added ping_i2c as demo for i2c dev. driver, using clearBufferI2C

--- Phase_II/I2C.X/main.c
+++ Phase_II/I2C.X/main.c
@@ -1,102 +1,87 @@
 /* 
  * File:   main.c
- * Author: André Gradim
+ * Author: Andrï¿½ Gradim
  *
  * Created on March 26, 2018, 10:32 AM
  */
 
-#include "header_pragma.h"
+#include "../DEVCFGx_config_bits.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include "uart.h"
+#include "../interrupts.h"
+#include "../UART.X/uart1.h"
 #include "i2c2.h"
 
-//void initI2C2();
 
-/*
- * 
- */
 int main(int argc, char** argv) {
     
-    configureUART1(115200,40000000);
+    config_UART1(115200, 8, 'N', 1);
+
+    ENABLE_UART1_PHERIPHERAL;
+    flush_RX_buffer();
+    flush_TX_buffer();
+    ENABLE_UART1_ERROR_DETECTION_INT;
+    ENABLE_UART1_TX_INT;
+    ENABLE_UART1_RX_INT;
+    
+    configure_global_interrupts();
+    Enable_Global_Interrupts();
+    
+    
     openI2C2();
+
+    put_string("Number of devices connected: ");
+    uint8_t array[254];
+    uint8_t count = i2c_ping(array);
+    //uint8_t count = 0;
+    //array[0] = 0x29;
+    //array[1] = 0x67;
     
+    put_string("Number of devices connected: ");
+    put_uint8(count);
+    put_char('\n');
+    //put_char(((*array>>4) & 0x0F) + '0');
+    //       put_char((*array & 0x0F) + '0');
+    //put_char('\n');
     
+    if(count > 0){
+        put_string("List of addresses: \n");
+        uint8_t index = 0;
+        for(index = 0; index < count; index++){
+            put_uint8(index);
+            put_string(" -> 0x");
+            put_char(((array[index]>>4) & 0x0F) + '0');
+            put_char((array[index] & 0x0F) + '0');
+            put_char('\n');
+        }
+    }else{
+        put_string("No device connected to the I2C bus\n");
+    }
     
-    while(1){   
-        putChar('R');
-        char in = getChar();
+    //uint8_t temp1=0;
+    char aux;
+    while(get_char(&aux) == 1 );
+    
+    if(count > 0){
+        clearI2CBuffer();
+        uint8_t temp1 = 0;
+
+        if(readByte(array[0],0x75,&temp1)){
+            put_string("ERROR: read problem");
+        }
+
+        put_string("Value: ");
+        put_uint16(temp1);
+        put_char('\t');
+        put_char((temp1>>4 < 9)?(((temp1>>4) & 0x0F) + '0'):((temp1>>4) & 0x0F) + 'A'-10);
+        put_char((temp1 & 0x0F) + '0');
+        put_char('\n');
+    }
+    
+   
         
-        
-        /////////////////////////////////
-        /*putChar('w'); 
-        
-        waitI2C2();
-        startI2C2();
-        
-        uint8_t dev_address = 0x68;
-        uint8_t buf_size = 2;
-        uint8_t transfer_buf[] = {0x71, 0x01};
-                
-        masterSend(dev_address, transfer_buf, buf_size);
-        
-        waitI2C2();
-        stopI2C2();
-        
-        */
-        
-        ///////////////////////////////
-        putChar('r');
-        
-        waitI2C2();
-        startI2C2();
-        
-        uint8_t dev_address = 0x68;
-        uint8_t buf_size = 1;
-        uint8_t transfer_buf2[] = {0x19};
-                
-        masterSend(dev_address, transfer_buf2, buf_size);
-        
-        waitI2C2();
-        stopI2C2();
-        
-        ///
-        
-        waitI2C2();
-        startI2C2();
-        dev_address = 0x68;
-        
-        masterSend(dev_address, NULL, 0);
-        
-        
-        uint8_t bytes_to_read = 1;
-        uint8_t buffer[bytes_to_read];
-        
-        masterReceive( buffer, bytes_to_read);
-        
-        waitI2C2();
-        stopI2C2();
-        
-        /////////////////////////////////
-        putChar('w'); 
-        
-        waitI2C2();
-        startI2C2();
-        
-        dev_address = 0x68;
-        buf_size = 2;
-        uint8_t transfer_buf[] = {0x19, 0x22};
-                
-        masterSend(dev_address, transfer_buf, buf_size);
-        
-        waitI2C2();
-        stopI2C2();
-        
-       
-        
-        
-        
+    while(1){
         
         
     }
