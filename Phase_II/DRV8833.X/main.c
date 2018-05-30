2e86d28a5b502496eb1ddb370efcac70357772e7
Pedro Martins <martinspedro@ua.pt>
2018-05-30 13:19:15 +0100

    Fixed minor bugs on movement
    Added Weak pull up to nFault
    function headears for movement function made public

--- Phase_II/DRV8833.X/main.c
+++ Phase_II/DRV8833.X/main.c
@@ -6,6 +6,9 @@
  * \date   Created on May 6, 2018, 2:40 PM
  */
 
+#include "../DEVCFGx_config_bits.h"
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -16,8 +19,8 @@ void main(void)
     configure_DRV8833_interface();
     enable_DRV8833();
     
-    forward_fast_decay_left(10);
-    forward_fast_decay_right(10);
+    forward_fast_decay_left(30);
+    forward_fast_decay_right(30);
     
    
     while(1);
