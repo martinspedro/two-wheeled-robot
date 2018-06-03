7da7c953a8e5e7e2d56f88cfa9105e941856e498
Pedro Martins <martinspedro@ua.pt>
2018-06-03 15:37:41 +0100

    Impoved Timer 3 Documentation and Modularity

--- Phase_II/ADC.X/timer3.c
+++ Phase_II/ADC.X/timer3.c
@@ -1,60 +1,45 @@
 /** 
  * \file   timer3.c
  * \brief  Timer 3 Module C configuration class file
- * 
- * The Timer 3 is the timer dedicated to the Output and Compare Peripheral, used
- * as a PWM Module
+ * \remarks Timer 3 is the timer dedicated to the Analog to Digital Converter (ADC)
  * 
  * \author Pedro Martins
  * \date   Created on May 28, 2018, 00:13 AM
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
-#include "../global.h"
+
 #include "../interrupts.h"
+
 #include "timer3.h"
-#include "../UART.X/uart1.h"
+
+
 /*******************************************************************************
  *                          MACROS DEFINITION
  ******************************************************************************/
 #define TIMER3_IPL  2        //!< Interrupt Priority Level for TIMER 3
 #define TIMER3_IPSL 1        //!< Interrupt Sub Priority Level for TIMER 3
 
+#define CLEAR_TMR3_IF {IFS0bits.T3IF = 0;}    //!< Clear Timer 3 Interrupt Flag
+#define CLEAR_TMR3 {TMR3 = 0;}                //!< Clear Timer 3 Count Register
 
-#define CLEAR_TMR3_INT_FLAG {IFS0bits.T3IF = 0;}
-#define CLEAR_TMR3 {TMR3 = 0;}
 
 /*******************************************************************************
  *                         CLASS METHODS
  ******************************************************************************/
 
-/* Select independent operation from Timer 2 and 3 is selected on TImer 2*/
 void config_Timer3(void)
 {
     DISABLE_TIMER_3;
     DISABLE_TIMER3_INTERRUPTS;
     CLEAR_TMR3;
-    CLEAR_TMR3_INT_FLAG;
+    CLEAR_TMR3_IF;
     
-    /* Enable Timer3 module operation when CPU enters IDLE mode
-     * 
-     * Stop in IDLE Mode bit
-     *   1 = Discontinue operation when CPU enters IDLE mode
-     *   0 = Continue operation in IDLE mode
-     */
+    /* Enable Timer3 module operation when CPU enters IDLE mode */
     T3CONbits.SIDL = 0;
     
-    /* Disable Timer 3 Gate Accumulation
-     * 
-     * Timer Gated Time Accumulation Enable bit
-     *   When TCS = 1:
-     *      This bit is ignored and reads as ?0?
-     *   When TCS = ?0?:
-     *      1 = Gated time accumulation is enabled
-     *      0 = Gated time accumulation is disabled
-     */
+    /* Disable Timer 3 Gate Accumulation */
     T3CONbits.TGATE = 0;
     
     /* Select 1:1 prescaler
@@ -72,31 +57,38 @@ void config_Timer3(void)
     T3CONbits.TCKPS = 0b000;
     
      
-    /* Select Internal Peripheral Clock as Clock Source
-     * 
-     * TMR3 Clock Source Select bit
-     *   1 = External clock from T2CK pin
-     *   0 = Internal peripheral clock
-     */
+    /* Select Internal Peripheral Clock as Clock Source */
     T3CONbits.TCS = 0;
     
-    /* Set Period of PWM
-     * 
-     * Period Register
-     *    PR<15:0>: 16-bit Timer2 period match value. Provides lower half of the
-     *              32-bit period match value when Timer2 and Timer3 are 
-     *              configured to form a 32-bit timer.
-     */
+    /* Set Timer Period */
      PR3 = PR3_PERIOD;
      
+     /* Set Interrupt Priority Level and Sublevel */
      IPC3bits.T3IP = TIMER3_IPL;
      IPC3bits.T3IS = TIMER3_IPSL;
 }
 
+
+
+/** \brief ISR for Timer 3
+ * 
+ * \pre    Global Interrupts must be configured and enabled
+ * \pre    Timer 3 must be configured and enabled
+ * 
+ * \remarks This interrupt service routine is empty because it is not executed
+ * 
+ * \param  None.
+ * \return None.
+ * 
+ * Interruption Service Routine for Timer 3 (TMR3)
+ * This Timer is associated with the ADC module
+ * 
+ * The Interruption Priority Level is 2 and the compiler must write software to
+ * perform the context switching (IPL2SOFT)
+ * 
+ * \author Pedro Martins
+ */
 void __ISR(_TIMER_3_VECTOR, IPL2SOFT) TIMER3_ISR(void)
 {
-   // LATDbits.LATD4 = 1;
-    CLEAR_TMR3_INT_FLAG;
-   // LATDbits.LATD4 = 0;
-
+    CLEAR_TMR3_IF;
 }
--- Phase_II/ADC.X/timer3.h
+++ Phase_II/ADC.X/timer3.h
@@ -1,8 +1,10 @@
-/* 
- * File:   timer2.h
- * Author: martinspedro
- *
- * Created on May 5, 28, 2018, 12:09 AM
+/** 
+ * \file   timer3.h
+ * \brief  Timer 3 Module Header class file
+ * \remarks Timer 3 is the timer dedicated to the Analog to Digital Converter (ADC)
+ * 
+ * \author Pedro Martins
+ * \date   Created on May 28, 2018, 12:09 AM
  */
 
 #include <xc.h>
@@ -10,19 +12,34 @@
 #ifndef TIMER3_H
 #define	TIMER3_H
 
-#define ENABLE_TIMER_3  {T3CONbits.ON = 1;}
-#define DISABLE_TIMER_3 {T3CONbits.ON = 0;}
+/*******************************************************************************
+ *                          MACROS DEFINITION
+ ******************************************************************************/
+#define ENABLE_TIMER_3  {T3CONbits.ON = 1;}     //!< Enables Timer 3 Peripheral
+#define DISABLE_TIMER_3 {T3CONbits.ON = 0;}     //!< Disables Timer 3 Peripheral
 
-#define ENABLE_TIMER3_INTERRUPTS  {IEC0bits.T3IE = 1;}
-#define DISABLE_TIMER3_INTERRUPTS {IEC0bits.T3IE = 0;}
+#define ENABLE_TIMER3_INTERRUPTS  {IEC0bits.T3IE = 1;}       //!< Enables Timer 3 Interruptions
+#define DISABLE_TIMER3_INTERRUPTS {IEC0bits.T3IE = 0;}       //!< Disables Timer 3 Interruptions
+
+#define PR3_PERIOD 50000        //!< Timer 3 Period Register Value
 
 
-#define PR3_PERIOD 50000
 
+/*******************************************************************************
+ *                        FUNCTION HEADERS DEFINITION
+ ******************************************************************************/
 
+/** \brief Configures Timer 3
+ * 
+ * Configures Timer 3 as a 16 bit timer with the Peripheral Clock as Clock Source
+ * 
+ * \remarks The independent operation of Timer 2 and 3 is selected on Timer 2 configuration
+ * 
+ * \param  None.
+ * \return None.
+ * 
+ * \author Pedro Martins
+ */
 void config_Timer3(void);
 
 #endif	/* TIMER3_H */
-
-
-
